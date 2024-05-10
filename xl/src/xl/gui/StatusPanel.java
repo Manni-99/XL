cfa05d2c426189cf6d54686bb82f373b3f24d3e7
Svante Johansson <Jsvante94@gmail.com>
2024-05-10 16:08:21 +0200

    adresser displayas med musen

--- xl/src/xl/gui/StatusPanel.java
+++ xl/src/xl/gui/StatusPanel.java
@@ -3,10 +3,14 @@ package xl.gui;
 import static java.awt.BorderLayout.CENTER;
 import static java.awt.BorderLayout.WEST;
 
-public class StatusPanel extends BorderPanel {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StatusPanel extends BorderPanel{
 
     protected StatusPanel(StatusLabel statusLabel) {
         add(WEST, new CurrentLabel());
         add(CENTER, statusLabel);
     }
+
 }
