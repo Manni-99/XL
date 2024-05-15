d2f0c79c9c40d8da71c2a9546269030a815971d8
Kristmann Thorsteinsson <Kristmann909@gmail.com>
2024-05-15 11:04:19 +0200

    Updated sheet class

--- xl/src/xl/model/Sheet.java
+++ xl/src/xl/model/Sheet.java
@@ -6,53 +6,56 @@ import xl.model.expr.Environment;
 import xl.model.expr.Expr;
 import xl.model.expr.ExprParser;
 
-public class Sheet implements Environment{
+public class Sheet implements Environment, Cell{
     private Map<String, Cell> cells = new HashMap<>();
     private ExprParser checker;
 
-    public Sheet(int row, int columns) {
-        
-
+    public Sheet() {
+        //Not yet implemented
     }
-    /* 
-    public boolean add(String ref, String value) {
+     
+    public boolean add(String ref, String value) throws IOException {
         // 1) Make an exprParser and expr
         checker = new ExprParser();
+
         Expr newCell = new Expr() {
 
             @Override
             public String toString(int prec) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'toString'");
+                return ref;
             }
 
             @Override
             public double value(Environment env) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'value'");
+                return env.value(ref);
             }
             
         };
+        
         // 2) Save the old value
-         var oldCell = checker.build(cells.get(ref)); // We are getting the value of the reference inside our hash map
-                                                     // and parsing the value
+         var oldCell = checker.build(value);              // We are getting the value of the reference inside our hash map
+                                                         // and parsing the value
         // 3) Insert the bombcell
-        cells.replace(ref, value);
+        cells.put(ref, (Cell) newCell);
         // 4) Provberäkna mappen
         for (var entry : cells.entrySet()) {
-            if (Double.isInfinite(checker.build(entry.getValue())) ||
-                    newCell.value(null) == entry.getValue() && entry.getKey() == newCell.value(null)) {
-                cells.replace(ref, oldCell);
+            if (Double.isInfinite(value(entry.getKey())) ||
+                    newCell == entry.getValue() && entry.getValue() == newCell) {
+                cells.replace(ref, (Cell) oldCell);
                 return false;
             }
+        }
             // 5) Insert the new value
+            
+            //cells.put(ref, (Cell) newCell);
 
             // 6) Calculate all cells with a value
             for (var entry1 : cells.entrySet()) {
                 for (var entry2 : cells.entrySet()) {
-                    if (Double.isInfinite(checker.build(entry2.getValue())) ||
-                            entry1.getKey() == entry2.getValue() && entry2.getKey() == entry1.getValue()) {
-                        cells.replace(ref, oldCell);
+                    if (Double.isInfinite((value(entry2.getKey()))) ||
+                            checker.build(entry1.getKey()) == entry2.getValue() 
+                            && checker.build(entry2.getKey()) == entry1.getValue()) {
+                        cells.replace(ref, (Cell) oldCell);
                         return false;
                     }
                 }
@@ -60,8 +63,7 @@ public class Sheet implements Environment{
             return true;
         }
         
-    }
-    */
+    
     public String load(InputStream filePath) throws IOException{
         StringBuilder sb = new StringBuilder();
         try(BufferedReader br 
@@ -75,7 +77,6 @@ public class Sheet implements Environment{
     }
 
     public boolean save(String filePath){
-        
         try{
             File myFile = new File(filePath);
             if(myFile.createNewFile()){
@@ -87,26 +88,44 @@ public class Sheet implements Environment{
         return false;
     }
 
-    public String display(String variable){
-        
-        return null;
-    }
-
-    public String formula(String formula){
-        return null;
-    }
-
     public void clear(String cell){
-
-
+        for(var c : cells.entrySet()){
+            if(c.getKey().equals(cell)){
+                cells.remove(cell);
+                System.out.println(cell + " Has been removed"); //Test prints
+            }
+        }
+        System.out.println("Nothing has been removed");//Test prints
     }
 
-    public void clearAll(String allCells){
-
+    public void clearAll(){
+       cells.clear();
+    }
+    @Override
+    public <E> String display(E ref) {
+        return null;
     }
 
-    public double value(String cellValue){
+    @Override
+    public <E> String formula(E e) {
+        return null;
+    }
+
+    @Override   //Cell implementation
+    public <E> double value(E ref) {
+        return cells.get(ref).value(ref);
+    }
+
+    @Override   //Environment implementation
+    public double value(String name) {
+        try {
+        Expr ex = checker.build(name);
+            return value(ex);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return 0.0;
     }
+
 
 }
