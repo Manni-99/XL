bef2ac2907308e77211136a4ec547eda9e34bb6e
Kristmann Thorsteinsson <Kristmann909@gmail.com>
2024-05-17 16:08:05 +0200

    Updates

--- xl/src/xl/model/BombCell.java
+++ xl/src/xl/model/BombCell.java
@@ -1,15 +1,28 @@
 package xl.model;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import xl.model.expr.Environment;
 import xl.model.expr.Expr;
+import xl.model.expr.ExprParser;
 
 public class BombCell implements Cell {
     private Object content; // Store the content of the cell before the bomb cell is inserted
+    private List<String> dependentRef;
+    private ExprParser parser;
 
     public BombCell(Object content) {
         this.content = content;
-    }
+        this.dependentRef = new ArrayList<>();
+        this.parser = new ExprParser();
 
+    }
+    public BombCell(Object content, List<String> dependentRef ){
+        this.content = content;
+        this.dependentRef = new ArrayList<>(dependentRef); // Copy constructor for ArrayList
+        this.parser = new ExprParser();
+    }
     @Override
     public <E> String display(E e) {
         return ""; // Return an empty string for display
@@ -31,10 +44,13 @@ public class BombCell implements Cell {
             } else if (content instanceof String) {
                 // Convert the String to a double and return
                 try {
-                    return Double.parseDouble((String) content);
+                    Expr ex = parser.build((String) content);
+                    return ex.value(env);
                 } catch (NumberFormatException ex) {
                     // If the String cannot be parsed to a double, return 0
                     return 0.0;
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             } else {
                 throw new IllegalArgumentException("Unsupported content type: " + content.getClass());
@@ -42,10 +58,23 @@ public class BombCell implements Cell {
         } else {
             throw new IllegalArgumentException("Environment object required for value calculation");
         }
+        return 0;
     }
 
     public Object getContent() {
         return content;
+    }
+
+    public List<String> getDependantRef() {
+        return dependentRef;
+    }
+
+    public void addDependentRef(String reference) {
+        dependentRef.add(reference);
+    }
+
+    public void removeDependentRef(String reference) {
+        dependentRef.remove(reference);
     }
 }
 
--- xl/src/xl/model/CommentCell.java
+++ xl/src/xl/model/CommentCell.java
@@ -1,24 +1,21 @@
 package xl.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
 import xl.model.expr.Environment;
+import xl.model.expr.Expr;
+import xl.model.expr.ExprParser;
 
 public class CommentCell implements Cell{
 
     private String comment;
-    private List<String> dependentRef;
+    private ExprParser parser;
 
     public CommentCell(String comment) {
         this.comment = comment;
-        this.dependentRef = new ArrayList<>();
+        this.parser = new ExprParser();
     }
 
-    public CommentCell(String comment, List<String> dependentRef) {
-        this.comment = comment;
-        this.dependentRef = new ArrayList<>(dependentRef); // Copy constructor for ArrayList
-    }
     @Override
     public <E> String display(E e) {
         return comment;
@@ -31,23 +28,32 @@ public class CommentCell implements Cell{
     }
 
 
-    @Override
-    public <E> double value(E e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'value'");
+     @Override
+    public double value(Object o) {
+        // Evaluate the content using the provided environment
+        if (o instanceof Environment) {
+            Environment env = (Environment) o;
+             if (comment instanceof String) {
+                // Convert the String to a double and return
+                try {
+                    Expr ex = parser.build(comment);
+                    return ex.value(env);
+                } catch (NumberFormatException ex) {
+                    // If the String cannot be parsed to a double, return 0
+                    return 0.0;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                throw new IllegalArgumentException("Unsupported content type: " + comment.getClass());
+            }
+        } else {
+            throw new IllegalArgumentException("Environment object required for value calculation");
+        }
+        return 0;
     }
 
-    public List<String> getDependantRef() {
-        return dependentRef;
-    }
-
-    public void addDependentRef(String reference) {
-        dependentRef.add(reference);
-    }
-
-    public void removeDependentRef(String reference) {
-        dependentRef.remove(reference);
-    }
+    
     public String toString(){
         return comment;
     }
