f0211927e0dd27097d798decdc510ee2ba24c9db
Erik Skeppner <skeppner.erik@gmail.com>
2024-05-20 11:08:10 +0200

    clearAll Gjord, behöver sheet för slutsteg

--- xl/src/xl/gui/menu/ClearAllMenuItem.java
+++ xl/src/xl/gui/menu/ClearAllMenuItem.java
@@ -3,10 +3,8 @@ package xl.gui.menu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JMenuItem;
-import javax.swing.JPanel;
 
 import xl.gui.*;
-import xl.model.*;
 
 class ClearAllMenuItem extends JMenuItem implements ActionListener {
 
@@ -22,10 +20,9 @@ class ClearAllMenuItem extends JMenuItem implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
-        // TODO
+        // TODO behöver läggas till metod eller lösning som raderar alla referanser
+        // från sheet så den och alla listor vi skapar med input är tomma
 
-        // när vald töm alla rutor
-        // -- göras genom att köra main kommando igen?
         ms.resetUpdaterXL();
         reset();
         System.out.println("allt clear");
@@ -34,7 +31,9 @@ class ClearAllMenuItem extends JMenuItem implements ActionListener {
     public void reset() {
 
         for (SlotLabel slot : sheetPanel.getSlotLabels().getLabels()) {
-            slot.setText(null);
+            ms.setLastClicked(slot);
+            ms.setSlotInputFromEditor(null);
+            // slot.setText(null);
             slot.ClearColor();
         }
     }
